53b8ba20ef87d43c311b35357b79de54a857c564
Md Akhtaruzzaman Sumon <146063221+dev-sumon@users.noreply.github.com>
2024-08-01 10:15:24 +0600

    dashboard setup done

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('admin-page', function(){
+//     return view('backend.index');
+// });
