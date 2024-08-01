28e0de0539f43d23218c359d184438aa9f9129d2
Md Akhtaruzzaman Sumon <146063221+dev-sumon@users.noreply.github.com>
2024-08-01 10:36:49 +0600

    some update

--- resources/views/backend/dashboard/dashboard.blade.php
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -9,12 +9,6 @@
             <div class="col-sm-6">
                 <h1 class="m-0">Dashboard</h1>
             </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard v1</li>
-                </ol>
-            </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
@@ -30,7 +24,6 @@
                 <div class="small-box bg-info">
                     <div class="inner">
                         <h3>150</h3>
-
                         <p>New Orders</p>
                     </div>
                     <div class="icon">
