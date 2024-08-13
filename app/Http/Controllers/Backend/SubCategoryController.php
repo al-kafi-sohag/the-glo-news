af3db634a0e5fa18bb25e0ef5f3adc1d95ad68a4
al-kafi-sohag <aksohag16@gmail.com>
2024-08-13 22:24:33 +0600

    Update SubCategoryController.php

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -49,8 +49,8 @@ class SubCategoryController extends Controller
 
                 $from_path = $temp_file->path . '/' . $temp_file->filename;
                 $to_path = 'images/sub-category/' . $save->id . '/' . $temp_file->filename;
-                Storage::setVisibility($to_path, 'public');
                 Storage::move($from_path, 'public/'.$to_path);
+                Storage::setVisibility($to_path, 'public');
                 Storage::deleteDirectory($temp_file->path);
 
                 $save->img = $to_path;
