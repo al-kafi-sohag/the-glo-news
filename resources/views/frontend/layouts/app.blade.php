df3f8b7f0391d8e8772e884a2f2d1f21458c6560
al-kafi-sohag <aksohag16@gmail.com>
2024-09-02 13:51:35 +0600

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="zAElhakSlUOTTEvBcSBoD3st8Y98mrWK6XTSmJUmAFE" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>
