f2ba0150b6f871eed1de4c3de047f106dd6a2516
Md Akhtaruzzaman Sumon <146063221+dev-sumon@users.noreply.github.com>
2024-09-05 19:36:46 +0600

    some update

--- resources/views/backend/include/nav_top.blade.php
+++ resources/views/backend/include/nav_top.blade.php
@@ -13,11 +13,6 @@
             </a>
         </li>
         <li class="nav-item dropdown user-menu">
-            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://assets.infyom.com/logo/blue_logo_150x150.png" class="user-image img-circle elevation-2"
-                    alt="User Image">
-                {{-- <span class="d-none d-md-inline">{{ Auth::user()->name }}</span> --}}
-            </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu">
                 <!-- User image -->
                 <li class="user-header bg-primary">
