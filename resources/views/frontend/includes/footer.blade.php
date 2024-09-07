c5f46aad9f71954a87cfaef51f1ba45ce63b6bae
Md Akhtaruzzaman Sumon <146063221+dev-sumon@users.noreply.github.com>
2024-09-08 00:05:16 +0600

    some update

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -53,10 +53,10 @@
     <div class="footer_bar_wrapper">
         <div class="menu-top-menu-container">
             <ul id="footer_menu" class="footer_nav">
-                <li class="menu-item"><a href="index.html">Home</a></li>
+                <li class="menu-item"><a href="{{ route('f.home') }}">Home</a></li>
                 <li class="menu-item"><a href="index.html">About Us</a></li>
                 <li class="menu-item"><a href="index.html">Advertisement</a></li>
-                <li class="menu-item"><a href="contact.html">Contact</a></li>
+                <li class="menu-item"><a href="{{ route('f.contact.index') }}">Contact</a></li>
                 <li class="menu-item">
                     @if (auth()->user())
                     <a target="_blank" href="{{ route('b.dashboard') }}">Dashboard</a>
