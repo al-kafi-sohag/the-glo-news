4d54251bd68d23b10153ae949305509794d7d455
al-kafi-sohag <aksohag16@gmail.com>
2024-10-01 20:11:45 +0600

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,25 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:4',
+        ]);
+
+        if (Auth::attempt($credentials)){
+            $user = Auth::user();
+            if ($user->status != 1) {
+                Auth::logout();
+                return redirect()->back()->withErrors(['error' => 'Your account is not active. Please contact an administrator.']);
+            }
+
+            return redirect()->intended($this->redirectTo);
+        }
+
+        return back()->withErrors(['error' => 'Invalid credentials']);
     }
 }
