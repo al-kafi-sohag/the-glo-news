bc68365e1897af960ab0a520bb868ca4ecf801c0
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-02-06 10:57:23 +0600

    Update MultipleNewsController.php

--- app/Http/Controllers/Frontend/MultipleNewsController.php
+++ app/Http/Controllers/Frontend/MultipleNewsController.php
@@ -15,37 +15,17 @@ class MultipleNewsController extends Controller
 {
     public function index($category_slug, $sub_category_slug = false): View
     {
-        $data['category'] = Category::with('subCategories')
-            ->where('slug', $category_slug)
-            ->activated()
-            ->first();
-
-        if ($sub_category_slug) {
-            $data['sub_category'] = SubCategory::with('category')
-                ->where('slug', $sub_category_slug)
-                ->activated()
-                ->first();
+        $data['category'] = Category::with('subCategories')->where('slug', $category_slug)->activated()->first();
+        if($sub_category_slug){
+            $data['sub_category']  = SubCategory::with('category')->where('slug', $sub_category_slug)->activated()->first();
         }
+        $query=PostCategory::with('post.author', 'category', 'subCategory')->where('category_id', $data['category']->id);
 
-        $data['news'] = Post::with([
-            'author',
-            'postCategories.category',
-            'postCategories.subCategory'
-        ])
-        ->activated()
-        ->whereHas('postCategories', function ($query) use ($data) {
-            $query->where('category_id', $data['category']->id);
-            if (isset($data['sub_category'])) {
-                $query->where('subcategory_id', $data['sub_category']->id);
-            }
-        })
-        // ->orderBy('order', 'asc')
-        ->latest()
-        ->paginate(12);
-
-
-        return view('frontend.news.multiple', $data);
-
+        if(isset($data['sub_category'])){
+            $query->where('subcategory_id',$data['sub_category']->id);
+        }
+        $data['news'] = $query->paginate(10);
+        return view('frontend.news.multiple',$data);
     }
 
 }
