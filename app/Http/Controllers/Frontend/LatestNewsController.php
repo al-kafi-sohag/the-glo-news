e6d2ac0cff2f20e8ca255bc424625b42d34c42b8
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2026-02-17 23:37:06 +0600

    Update LatestNewsController.php

--- app/Http/Controllers/Frontend/LatestNewsController.php
+++ app/Http/Controllers/Frontend/LatestNewsController.php
@@ -11,7 +11,7 @@ class LatestNewsController extends Controller
 
     public function index($type='all')
     {
-        $post_per_page = 10;
+        $post_per_page = 25;
 
         $query = Post::with('author:id,name')->activated()
             ->orderByRaw('CASE WHEN `order` IS NOT NULL THEN 0 ELSE 1 END')
